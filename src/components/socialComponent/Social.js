af4b963a9596dd33f17332e62c0e5a04484f2a7d
Quinton Fults <quinton.fults.work@gmail.com>
2018-10-29 23:59:17 -0700

    boilerplate for social descriptors

--- src/components/socialComponent/Social.js
+++ src/components/socialComponent/Social.js
@@ -6,9 +6,17 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import Paper from '@material-ui/core/Paper'
 import YouTubePlayer from '../youtubeComponent/youtube'
 import Form from '../formComponent/Form'
 const styles = theme => ({
+  rootPaper: {
+    ...theme.mixins.gutters(),
+    margin: 0,
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 1,
+    backgroundColor: 'white'
+  },
   root: {
     width: '100%',
     background: '#212121'
@@ -33,6 +41,11 @@ const styles = theme => ({
   },
   handle: {
     fontSize: theme.typography.pxToRem(10)
+  },
+  stock: {
+    width: '12rem',
+    height: '10rem',
+    borderRadius: '4%'
   }
 })
 
@@ -43,12 +56,12 @@ const Social = props =>
         <Typography className={props.classes.heading}>YouTube <span className={props.classes.handle}><i>@xeleratorTube</i></span></Typography>
       </ExpansionPanelSummary>
       <ExpansionPanelDetails>
-        <Typography>
-          <YouTubePlayer />
-          {/* <iframe title='sample' className={props.classes.video} width='560' height='315' src='https://www.youtube.com/embed/VuN8qwZoego?controls=0' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen />. */}
-          <a href='https://www.youtube.com/channel/UCoebwHSTvwalADTJhps0emA'>Here</a>
-          <span> you will find all of our available videos for free tier subscribers.</span>
-        </Typography>
+        <Paper className={props.classes.rootPaper} elevation={1}>
+          <Typography>
+            <YouTubePlayer />
+            <p style={{ paddingTop: '0.2rem' }}>YouTube is where you can find any and all Xelerator videos, both for Free Tier and Premium Tier subscribers. Check out the video above for an explanation all about Xelerator and what we do!</p>
+          </Typography>
+        </Paper>
       </ExpansionPanelDetails>
     </ExpansionPanel>
     <ExpansionPanel className={props.classes.panel}>
@@ -56,10 +69,12 @@ const Social = props =>
         <Typography className={props.classes.heading}>Instagram <span className={props.classes.handle}><i>@xeleratorInsta</i></span></Typography>
       </ExpansionPanelSummary>
       <ExpansionPanelDetails>
-        <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-          sit amet blandit leo lobortis eget.
-        </Typography>
+        <Paper className={props.classes.rootPaper} elevation={1}>
+          <Typography>
+            <img className={props.classes.stock} src={require('../../images/instaSTOCK.jpg')} alt='instagram stock' />
+            <p>Instagram is a perfect platform for short and concise 1-minute instructional videos on some of the smaller concepts essential to Computer Programming. On the Xelerator instagram, that is exactly what you will find.</p>
+          </Typography>
+        </Paper>
       </ExpansionPanelDetails>
     </ExpansionPanel>
     <ExpansionPanel className={props.classes.panel}>
@@ -67,10 +82,12 @@ const Social = props =>
         <Typography className={props.classes.heading}>Twitter <span className={props.classes.handle}><i>@xeleratorTweet</i></span></Typography>
       </ExpansionPanelSummary>
       <ExpansionPanelDetails>
-        <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-          sit amet blandit leo lobortis eget.
-        </Typography>
+        <Paper className={props.classes.rootPaper} elevation={1}>
+          <Typography>
+            <img className={props.classes.stock} src={require('../../images/instaSTOCK.jpg')} alt='instagram stock' />
+            <p>Twitter is a perfect platform for short and concise 1-minute instructional videos on some of the smaller concepts essential to Computer Programming. On the Xelerator instagram, that is exactly what you will find.</p>
+          </Typography>
+        </Paper>
       </ExpansionPanelDetails>
     </ExpansionPanel>
     <ExpansionPanel className={props.classes.panel}>
