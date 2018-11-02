c82365602cb197eb2b34ae0df1826e3508a802d9
Quinton Fults <quinton.fults.work@gmail.com>
2018-11-01 21:46:51 -0700

    fixed about

--- src/components/aboutComponent/About.js
+++ src/components/aboutComponent/About.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
-w
+import Grid from '@material-ui/core/Grid'
 import Avatar from '@material-ui/core/Avatar'
 
 const styles = theme => ({
